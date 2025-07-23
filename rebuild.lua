127014709764b05e66eb742e904ceffbedca3d7e
Baolong12355 <125375091+Baolong12355@users.noreply.github.com>
2025-07-23 20:14:44 +0700

    Update rebuild.lua

--- rebuild.lua
+++ rebuild.lua
@@ -48,7 +48,7 @@ end
 
 -- Cấu hình mặc định
 local defaultConfig = {
-    ["Macro Name"] = "event",
+    ["Macro Name"] = "kl",
     ["PlaceMode"] = "Rewrite",
     ["ForceRebuildEvenIfSold"] = true,
     ["MaxRebuildRetry"] = nil,
