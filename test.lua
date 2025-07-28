3083a21072920b40f52907834c36d1939d32c79e
Baolong12355 <125375091+Baolong12355@users.noreply.github.com>
2025-07-28 16:04:31 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -48,7 +48,7 @@ end
 
 -- Cấu hình mặc định
 local defaultConfig = {
-    ["Macro Name"] = "event",
+    ["Macro Name"] = "x",
     ["PlaceMode"] = "Rewrite",
     ["ForceRebuildEvenIfSold"] = false,
     ["MaxRebuildRetry"] = nil,
