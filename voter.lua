17c99940a50f993fe6b285fe974922187fbc6277
Baolong12355 <125375091+Baolong12355@users.noreply.github.com>
2025-09-20 08:48:59 +0700

    Update voter.lua

--- voter.lua
+++ voter.lua
@@ -1,4 +1,3 @@
-
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TeleportService = game:GetService("TeleportService")
@@ -6,11 +5,9 @@ local TeleportService = game:GetService("TeleportService")
 local player = Players.LocalPlayer
 local gui = player:WaitForChild("PlayerGui")
 
-
 local function normalize(str)
     return string.upper((str:gsub("%s+", " ")):gsub("^%s*(.-)%s*$", "%1"))
 end
-
 
 local function titleCase(str)
     return string.gsub(str, "(%w)(%w*)", function(first, rest)
@@ -18,27 +15,17 @@ local function titleCase(str)
     end)
 end
 
-
 local function teleportToLobby()
     local lobbyPlaceId = 9503261072
     TeleportService:Teleport(lobbyPlaceId)
 end
 
+if not getgenv().TDX_Config or not getgenv().TDX_Config.mapvoting then return end
 
-if not getgenv().TDX_Config or not getgenv().TDX_Config.mapvoting then
-    return
-end
-
-
-
-repeat
-    task.wait()
-until gui:FindFirstChild("Interface") and 
-      gui.Interface:FindFirstChild("GameInfoBar") and 
-      gui.Interface.GameInfoBar:FindFirstChild("MapVoting") and 
-      gui.Interface.GameInfoBar.MapVoting.Visible
-
-
+repeat task.wait() until gui:FindFirstChild("Interface")
+    and gui.Interface:FindFirstChild("GameInfoBar")
+    and gui.Interface.GameInfoBar:FindFirstChild("MapVoting")
+    and gui.Interface.GameInfoBar.MapVoting.Visible
 
 local targetMap = normalize(getgenv().TDX_Config.mapvoting)
 local mapScreens = workspace:WaitForChild("Game"):WaitForChild("MapVoting"):WaitForChild("VotingScreens")
@@ -59,31 +46,28 @@ for i = 1, 4 do
 end
 
 if not found then
-    
     local changeRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("MapChangeVoteCast")
     local changeGui = gui.Interface:WaitForChild("MapVotingScreen").Bottom.ChangeMap
-    
+
     while not changeGui.Disabled.Visible do
         changeRemote:FireServer(true)
         task.wait(0.5)
     end
-    
+
     teleportToLobby()
     return
 end
-
 
 local voteName = titleCase(getgenv().TDX_Config.mapvoting)
 local voteRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("MapVoteCast")
 local readyRemote = ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("MapVoteReady")
 
-
-local success, err = pcall(function()
+local success, _ = pcall(function()
     voteRemote:FireServer(voteName)
     task.wait(0.1)
     readyRemote:FireServer()
 end)
 
-if success then
+if not success then
     teleportToLobby()
 end
