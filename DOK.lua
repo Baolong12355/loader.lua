9245065973ec2e098d4b2a9dc56d58a63b8c572f
Baolong12355 <125375091+Baolong12355@users.noreply.github.com>
2025-09-26 20:12:53 +0700

    Update DOK.lua

--- DOK.lua
+++ DOK.lua
@@ -1,160 +1,109 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
 local LocalPlayer = Players.LocalPlayer
 local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
 
-local ClientFolder = PlayerScripts:WaitForChild("Client")
-local GameClassFolder = ClientFolder:WaitForChild("GameClass")
-local UserInputHandlerFolder = ClientFolder:WaitForChild("UserInputHandler")
-local FirstPersonHandlerFolder = UserInputHandlerFolder:WaitForChild("FirstPersonHandler")
-local FirstPersonAttackManagerFolder = FirstPersonHandlerFolder:WaitForChild("FirstPersonAttackManager")
-local CommonFolder = ReplicatedStorage.TDX_Shared:WaitForChild("Common")
+local TowerClass = require(PlayerScripts.Client.GameClass:WaitForChild("TowerClass"))
 
-local TowerClass = require(GameClassFolder:WaitForChild("TowerClass"))
-local FirstPersonHandler = require(FirstPersonHandlerFolder)
-local FirstPersonAttackManager = require(FirstPersonAttackManagerFolder)
-local FirstPersonAttackHandlerClass = require(FirstPersonAttackManagerFolder:WaitForChild("FirstPersonAttackHandlerClass"))
-local EnemyClass = require(GameClassFolder:WaitForChild("EnemyClass"))
-local ProjectileHandler = require(GameClassFolder:WaitForChild("ProjectileHandler"))
-local Enums = require(CommonFolder:WaitForChild("Enums"))
+-- === CÀI ĐẶT ===
+local NEW_SPLASH_RADIUS = 9999 -- Thay đổi bán kính sát thương lan ở đây
+local ENABLED = true
+-- =================
 
-local NEW_SPLASH_RADIUS = 9999
-local original_FirstPersonHandler_Begin = FirstPersonHandler.Begin
-local original_FirstPersonHandler_Stop = FirstPersonHandler.Stop
-
-_G.CurrentFPSControlledTower = nil
-local hasNoEnemySet = false
-local isHooked = false
-
-local function ApplyHooks()
-    if isHooked then return end
-    isHooked = true
-
-    local original_AttackHandler_Attack = FirstPersonAttackHandlerClass._Attack
-    FirstPersonAttackHandlerClass._Attack = function(self)
-        local currentTower = _G.CurrentFPSControlledTower
-        if not (currentTower and currentTower.DirectControlHandler and currentTower.DirectControlHandler:IsActive()) then
-            return original_AttackHandler_Attack(self)
-        end
-        local towerPosition = currentTower:GetTorsoPosition() or currentTower:GetPosition()
-        local furthestEnemy = nil
-        local maxProgress = -1
-        for _, enemy in pairs(EnemyClass.GetEnemies()) do
-            if enemy and enemy.IsAlive and not enemy.IsFakeEnemy and enemy:FirstPersonTargetable() then
-                local pathProgress = (pcall(function() return enemy.MovementHandler:GetPathPercentage() end) and enemy.MovementHandler:GetPathPercentage() or 0)
-                if pathProgress > maxProgress then
-                    maxProgress = pathProgress
-                    furthestEnemy = enemy
-                end
-            end
-        end
-        if furthestEnemy then
-            local targetPosition, hitPart
-            local headPart = furthestEnemy.Character and furthestEnemy.Character.GetHead and furthestEnemy.Character:GetHead()
-            if headPart then
-                targetPosition = headPart.Position
-                hitPart = headPart
-            else
-                targetPosition = furthestEnemy:GetTorsoPosition() or furthestEnemy:GetPosition()
-                hitPart = furthestEnemy.Character and furthestEnemy.Character:GetTorso()
-            end
-            if hitPart and targetPosition then
-                local hitNormal = (targetPosition - towerPosition).Unit
-                if self.IsProjectile then
-                    self:_AttackProjectile(hitPart, targetPosition, hitNormal)
-                else
-                    self:_AttackHitscan(hitPart, targetPosition, hitNormal)
-                end
-                self:_BurstAttackHandling()
-                FirstPersonHandler.Attacked(self.Index, self.UseAbilityName, self.AttackConfig.NoTriggerClientTowerAttacked)
-                return
-            end
-        end
-        return original_AttackHandler_Attack(self)
-    end
-    
-    local original_NewProjectile = ProjectileHandler.NewProjectile
-    ProjectileHandler.NewProjectile = function(initData)
-        if _G.CurrentFPSControlledTower and initData and initData.OriginHash == _G.CurrentFPSControlledTower.Hash then
-            local furthestEnemy = nil; local maxProgress = -1
-            for _, enemy in pairs(EnemyClass.GetEnemies()) do
-                if enemy and enemy.IsAlive and not enemy.IsFakeEnemy and enemy:FirstPersonTargetable() then
-                    local pathProgress = (pcall(function() return enemy.MovementHandler:GetPathPercentage() end) and enemy.MovementHandler:GetPathPercentage() or 0)
-                    if pathProgress > maxProgress then maxProgress = pathProgress; furthestEnemy = enemy end
-                end
-            end
-            if furthestEnemy then
-                initData.TargetHash = furthestEnemy.Hash
-                initData.TargetEntityClass = "Enemy"
-                initData.OverrideGoalPosition = furthestEnemy:GetTorsoPosition()
-                initData.ForceTrackCharacter = true
-            end
-        end
-        return original_NewProjectile(initData)
-    end
-
-    for _, mod in ipairs(getloadedmodules()) do
-        if mod.Name == "FirstPersonAttackHandlerClass" then
-            pcall(function()
-                local ModuleTable = require(mod)
-                if ModuleTable and ModuleTable.New then
-                    local oldNew = ModuleTable.New
-                    ModuleTable.New = function(...)
-                        local obj = oldNew(...)
-                        obj.DefaultShotInterval = 0.001
-                        obj.ReloadTime = 0.001
-                        obj.CurrentFirerateMultiplier = 0.001
-                        obj.DefaultSpreadDegrees = 0
-                        obj.DamageType = Enums.DamageTypes.Toxic 
-                        if obj.AttackConfig and obj.AttackConfig.DamageData and obj.AttackConfig.DamageData.StunData then
-                            obj.AttackConfig.DamageData.StunData.StunDuration = 99999999
-                        end
-                        return obj
-                    end
-                end
-            end)
-        elseif mod.Name == "FirstPersonCameraHandler" then
-            pcall(function()
-                local cameraMod = require(mod)
-                if cameraMod then
-                    if cameraMod.CameraShake then cameraMod.CameraShake = function() end end
-                    if cameraMod.ApplyRecoil then cameraMod.ApplyRecoil = function() end end
-                end
-            end)
-        end
-    end
-end
-
-FirstPersonHandler.Begin = function(towerInstance)
-    ApplyHooks() 
-    _G.CurrentFPSControlledTower = towerInstance
-    return original_FirstPersonHandler_Begin(towerInstance)
-end
-
-FirstPersonHandler.Stop = function(...)
-    if not _G.CurrentFPSControlledTower then return end
-    _G.CurrentFPSControlledTower = nil
-    FirstPersonAttackManager.ToggleTryAttacking(false)
-    hasNoEnemySet = false
-    return original_FirstPersonHandler_Stop(...)
-end
+print("Executor: Combat Drone Splash Damage script loaded.")
+print("Target Splash Radius: " .. tostring(NEW_SPLASH_RADIUS))
 
 RunService.Heartbeat:Connect(function()
-    pcall(function()
-        for hash, tower in pairs(TowerClass.GetTowers()) do
-            if tower and tower.Type == "Combat Drone" and tower.OwnedByLocalPlayer and tower.LevelHandler then
-                local levelStats = tower.LevelHandler:GetLevelStats()
-                if levelStats then
+    if not ENABLED then return end
+
+    local allTowers = TowerClass.GetTowers()
+    if not allTowers then return end
+
+    for hash, tower in pairs(allTowers) do
+        -- Thêm điều kiện: Chỉ áp dụng nếu trụ là "Combat Drone"
+        if tower and tower.Type == "Combat Drone" then
+            if tower.LevelHandler then
+                local success, levelStats = pcall(function()
+                    return tower.LevelHandler:GetLevelStats()
+                end)
+
+                if success and levelStats then
+                    -- Ghi đè các thuộc tính sát thương lan
                     levelStats.IsSplash = true
                     levelStats.SplashRadius = NEW_SPLASH_RADIUS
                 end
             end
         end
-    end)
+    end
+end)
 
-    if not _G.CurrentFPSControlledTower then return end
+
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local PlayerScripts = LocalPlayer:WaitForChild("PlayerScripts")
+
+local ClientFolder = PlayerScripts:WaitForChild("Client")
+local UserInputHandlerFolder = ClientFolder:WaitForChild("UserInputHandler")
+local FirstPersonHandlerFolder = UserInputHandlerFolder:WaitForChild("FirstPersonHandler")
+local FirstPersonAttackManagerFolder = FirstPersonHandlerFolder:WaitForChild("FirstPersonAttackManager")
+
+local FirstPersonHandler = require(FirstPersonHandlerFolder)
+local FirstPersonAttackManager = require(FirstPersonAttackManagerFolder)
+local FirstPersonAttackHandlerClass = require(FirstPersonAttackManagerFolder:WaitForChild("FirstPersonAttackHandlerClass"))
+local EnemyClass = require(ClientFolder:WaitForChild("GameClass"):WaitForChild("EnemyClass"))
+
+local original_FirstPersonHandler_Begin = FirstPersonHandler.Begin
+local original_FirstPersonHandler_Stop = FirstPersonHandler.Stop
+local original_AttackHandler_Attack = FirstPersonAttackHandlerClass._Attack
+
+_G.CurrentFPSControlledTower = nil
+_G.AutoAttackRunning = false
+local hasNoEnemySet = false -- track đã tắt attack khi không có enemy
+
+-- Begin / Stop Tower Control
+FirstPersonHandler.Begin = function(towerInstance)
+    _G.CurrentFPSControlledTower = towerInstance
+    _G.AutoAttackRunning = true
+    hasNoEnemySet = false
+    return original_FirstPersonHandler_Begin(towerInstance)
+end
+
+FirstPersonHandler.Stop = function()
+    _G.CurrentFPSControlledTower = nil
+    _G.AutoAttackRunning = false
+    FirstPersonAttackManager.ToggleTryAttacking(false)
+    hasNoEnemySet = false
+    return original_FirstPersonHandler_Stop()
+end
+
+-- Helper: tính tiến độ của enemy
+local function getEnemyPathProgress(enemy)
+    if not enemy or not enemy.MovementHandler then return 0 end
+    
+    if enemy.MovementHandler.GetPathPercentage then
+        return enemy.MovementHandler:GetPathPercentage() or 0
+    end
+    
+    if enemy.MovementHandler.PathPercentage then
+        return enemy.MovementHandler.PathPercentage or 0
+    end
+    
+    if enemy.MovementHandler.GetCurrentNode then
+        local currentNode = enemy.MovementHandler:GetCurrentNode()
+        if currentNode and currentNode.GetPercentageAlongPath then
+            return currentNode:GetPercentageAlongPath(1) or 0
+        end
+    end
+
+    return 0
+end
+
+-- Heartbeat: bật/tắt attack dựa vào enemy targetable, chỉ set false 1 lần khi không có enemy
+RunService.Heartbeat:Connect(function()
+    if not _G.AutoAttackRunning or not _G.CurrentFPSControlledTower then
+        return
+    end
 
     local foundEnemy = false
     for _, enemy in pairs(EnemyClass.GetEnemies()) do
@@ -168,7 +117,101 @@ RunService.Heartbeat:Connect(function()
         FirstPersonAttackManager.ToggleTryAttacking(true)
         hasNoEnemySet = false
     elseif not hasNoEnemySet then
+        -- chỉ set false 1 lần
         FirstPersonAttackManager.ToggleTryAttacking(false)
         hasNoEnemySet = true
     end
 end)
+
+-- Override _Attack: chọn enemy đi xa nhất + headshot 95%
+FirstPersonAttackHandlerClass._Attack = function(self)
+    local currentTower = _G.CurrentFPSControlledTower
+    if not (currentTower and currentTower.DirectControlHandler and currentTower.DirectControlHandler:IsActive()) then
+        return original_AttackHandler_Attack(self)
+    end
+
+    local towerPosition = currentTower:GetTorsoPosition() or currentTower:GetPosition()
+    local furthestEnemy = nil
+    local maxProgress = -1
+
+    for _, enemy in pairs(EnemyClass.GetEnemies()) do
+        if enemy and enemy.IsAlive and not enemy.IsFakeEnemy and enemy:FirstPersonTargetable() then
+            local enemyPosition = enemy:GetTorsoPosition() or enemy:GetPosition()
+            if enemyPosition and towerPosition then
+                local pathProgress = getEnemyPathProgress(enemy)
+                if pathProgress > maxProgress then
+                    maxProgress = pathProgress
+                    furthestEnemy = enemy
+                end
+            end
+        end
+    end
+
+    if furthestEnemy then
+        local targetChance = math.random(1, 100)
+        local targetPosition, hitPart
+
+        if targetChance <= 95 then -- headshot 95%
+            local headPart = furthestEnemy.Character and furthestEnemy.Character.GetHead and furthestEnemy.Character:GetHead()
+            if headPart then
+                targetPosition = headPart.Position
+                hitPart = headPart
+            else
+                targetPosition = furthestEnemy:GetTorsoPosition() or furthestEnemy:GetPosition()
+                hitPart = furthestEnemy.Character and furthestEnemy.Character:GetTorso() 
+                          or (furthestEnemy.Character and furthestEnemy.Character.PrimaryPart) 
+                          or (furthestEnemy.Model and furthestEnemy.Model.PrimaryPart)
+            end
+        else
+            targetPosition = furthestEnemy:GetTorsoPosition() or furthestEnemy:GetPosition()
+            hitPart = furthestEnemy.Character and furthestEnemy.Character:GetTorso() 
+                      or (furthestEnemy.Character and furthestEnemy.Character.PrimaryPart) 
+                      or (furthestEnemy.Model and furthestEnemy.Model.PrimaryPart)
+        end
+
+        if hitPart and targetPosition then
+            local hitNormal = (targetPosition - towerPosition).Unit
+            if self.IsProjectile then
+                self:_AttackProjectile(hitPart, targetPosition, hitNormal)
+            else
+                self:_AttackHitscan(hitPart, targetPosition, hitNormal)
+            end
+
+            self:_BurstAttackHandling()
+            FirstPersonHandler.Attacked(self.Index, self.UseAbilityName, self.AttackConfig.NoTriggerClientTowerAttacked)
+            return
+        end
+    end
+
+    return original_AttackHandler_Attack(self)
+end
+
+
+
+
+for _, mod in ipairs(getloadedmodules()) do
+    if mod.Name == "FirstPersonAttackHandlerClass" then
+        local ModuleTable = require(mod)
+        if ModuleTable and ModuleTable.New then
+            local oldNew = ModuleTable.New
+            ModuleTable.New = function(...)
+                local obj = oldNew(...)
+                obj.DefaultShotInterval = 0.001
+                obj.ReloadTime = 0.001
+                obj.CurrentFirerateMultiplier = 0.001
+                obj.DefaultSpreadDegrees = 0 -- No recoil/spread
+                return obj
+            end
+        end
+    elseif mod.Name == "FirstPersonCameraHandler" then
+        local cameraMod = require(mod)
+        if cameraMod and cameraMod.CameraShake then
+            cameraMod.CameraShake = function() end
+        end
+        if cameraMod and cameraMod.ApplyRecoil then
+            cameraMod.ApplyRecoil = function() end
+        end
+    end
+end
+
+print("Đã loại bỏ cooldown, tăng tốc bắn và loại bỏ độ giật!")
